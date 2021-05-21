81c4f94facfbfb045b09de3e52d786ed85e821a0
Andrey Kashcheev <a.kashcheev@f5.com>
2021-05-21 13:16:22 -0700

    ESECLDTPLT-2587: Telemetry Module

---
+++ automated-test-scripts/f5-azure-arm-templates-v2/examples/modules/bigip-autoscale/verify_telemetry_create_env.sh
@@ -0,0 +1,8 @@
+#  expectValue = "Succeeded"
+#  expectFailValue = "Failed"
+#  scriptTimeout = 3
+#  replayEnabled = true
+#  replayTimeout = 1800
+
+# Limit output, only report provisioningState
+az deployment group show -g <RESOURCE GROUP> -n <RESOURCE GROUP>-log-wrkspc | jq .properties.provisioningState
