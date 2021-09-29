8b0f1d6d4904dc1ac8d647069a9ecf208dc51aac
kashcheev <a.kashcheev@f5.com>
2021-09-29 20:55:46 +0000

    Release commited to v1.4.0.0 tag

--- automated-test-scripts/f5-azure-arm-templates-v2/examples/modules/bigip-standalone/verify_access_create_env.sh
+++
@@ -1,8 +0,0 @@
-#  expectValue = "Succeeded"
-#  expectFailValue = "Failed"
-#  scriptTimeout = 3
-#  replayEnabled = true
-#  replayTimeout = 1800
-
-# Limit output, only report provisioningState
-az deployment group show -g <RESOURCE GROUP> -n <RESOURCE GROUP>-access-env | jq .properties.provisioningState
