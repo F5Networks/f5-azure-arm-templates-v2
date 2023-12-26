2f15ef731676089cea7339d0f5ca7ecb3cab1be1
Michael Shimkus <m.shimkus@f5.com>
2023-12-26 08:39:56 -0800

    Resolve EC-392 "Release"

--- automated-test-scripts/f5-azure-arm-templates-v2/common/upload_templates.sh
+++ automated-test-scripts/f5-azure-arm-templates-v2/common/upload_templates.sh
@@ -5,7 +5,7 @@
 
 ## Create storage account  
 storage_account_name=$(echo st<RESOURCE GROUP>tmpl | tr -d -)
-storage_account_fqdn=$(az storage account create -n ${storage_account_name} -g <RESOURCE GROUP> -l <REGION> | jq -r .primaryEndpoints.blob)
+storage_account_fqdn=$(az storage account create -n ${storage_account_name} -g <RESOURCE GROUP> -l <REGION> --allow-blob-public-access true | jq -r .primaryEndpoints.blob)
 
 ## Create container
 storage_container=$(az storage container create -n templates --account-name ${storage_account_name} --public-access container | jq -r .created)
